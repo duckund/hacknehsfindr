df8ddf5364b0db2af2eac952fe387a9a07f9aadd
ducc <56479924+duckund@users.noreply.github.com>
2019-10-12 15:15:15 -0400

    Add files via upload

---
+++ php/submitform.php
@@ -0,0 +1,13 @@
+<html>
+	<body>
+		<hr>
+			
+			Name: <?php echo $_GET["myName"]; ?><br>
+			Contact: <?php echo $_GET["myEmail"]; ?><br>
+			Missing: <?php echo $_GET["myItem"]; ?><br><br>
+			Additional Notes:<br>
+			<?php echo $_GET["myTextArea"]; ?>
+		
+		<hr>
+	</body>
+</html>
